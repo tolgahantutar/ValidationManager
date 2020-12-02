74655892aede90ec621ede8ac2c3f4fcf653dbe1
htcblt <htc.bolat97@outlook.com>
2020-12-02 18:17:20 +0300

    phoneValidation

--- validationmanager/src/test/java/com/validationmanager/PhoneValidationTest.kt
+++ validationmanager/src/test/java/com/validationmanager/PhoneValidationTest.kt
@@ -1,4 +1,22 @@
 package com.validationmanager
 
+import com.validationmanager.library.validations.EmailValidation
+import com.validationmanager.library.validations.PhoneValidation
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Test
+
 class PhoneValidationTest {
+    @Test
+    fun test_phoneValidation_returnTrue(){
+        val result: Boolean = PhoneValidation().isPhoneValid("90,","05059639693")
+        MatcherAssert.assertThat(result, Matchers.equalTo(true))
+    }
+    @Test
+    fun test_phoneValidation_returnFalse(){
+        val result: Boolean = PhoneValidation().isPhoneValid("09,","050596396")
+        MatcherAssert.assertThat(result, Matchers.equalTo(false))
+    }
+
+
 }
