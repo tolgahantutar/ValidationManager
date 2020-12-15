575147dc94fcd878e2e18568df64cef862137421
Tolgahan TUTAR <tolgahan.tutar@gmail.com>
2020-12-15 10:45:21 +0300

    EmailValidation Updated

--- validationmanager/src/test/java/com/validationmanager/EmailValidationTest.kt
+++ validationmanager/src/test/java/com/validationmanager/EmailValidationTest.kt
@@ -8,12 +8,18 @@ import org.junit.Test
 class EmailValidationTest {
     @Test
     fun test_emailValidation_returnTrue(){
-        val result: Boolean = EmailValidation().isEmailValid("tolgahan.tutar@gmail.com")
+        val result: Boolean = EmailValidation.isEmailValid("tolgahan.tutar@gmail.com")
         MatcherAssert.assertThat(result, Matchers.equalTo(true))
     }
     @Test
     fun test_emailValidation_returnFalse(){
-        val result: Boolean = EmailValidation().isEmailValid("tolgahan.tutar@gmail-com")
+        val result: Boolean = EmailValidation.isEmailValid("tolgahan.tutar@gmail-com")
         MatcherAssert.assertThat(result, Matchers.equalTo(false))
     }
+    @Test
+    fun test_emailValidation_isValidateTurkishLetter_returnTrue(){
+        val result: Boolean = EmailValidation.isEmailValid("osman.gençoğlan@bexfa.com")
+        MatcherAssert.assertThat(result,Matchers.equalTo(true))
+    }
+
 }
